298b9b1ce801c1e305f6f2750789fe038b5dc5c4
Jed Liu <liujed@users.noreply.github.com>
2021-07-11 01:35:26 -0700

    Account for Rack's broken use of string encodings
    Rack assigns 8-bit ASCII encoding to all strings in its request
    environment. This causes issues when these strings have characters with
    the 8th bit set and are converted to UTF-8 for output to the HAR file.
    
    https://github.com/phusion/passenger/issues/1328

--- lib/akita/har_logger/har_utils.rb
+++ lib/akita/har_logger/har_utils.rb
@@ -3,11 +3,23 @@
 module Akita
   module HarLogger
     class HarUtils
+      # Rack apparently uses 8-bit ASCII for everything, even when the string
+      # is not 8-bit ASCII. This reinterprets the given string as the default
+      # external encoding and re-encodes into the default internal encoding.
+      def self.fixEncoding(v)
+        if v != nil then
+          v.encode(Encoding.default_internal, Encoding.default_external)
+        else
+          nil
+        end
+      end
+
       # Converts a Hash into a list of Hash objects. Each entry in the given
       # Hash will be represented in the output by a Hash object that maps
       # 'name' to the entry's key and 'value' to the entry's value.
       def self.hashToList(hash)
         hash.reduce([]) { |accum, (k, v)|
+
           accum.append({
             name: k,
             value: v,
