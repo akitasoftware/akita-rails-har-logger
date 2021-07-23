ec639d1a27ad53c4a577672b9e8eceb46dc44f74
Jed Liu <liujed@users.noreply.github.com>
2021-07-23 15:19:30 -0700

    Added debug logging around locks
    Bump to 0.2.12.

--- lib/akita/har_logger/writer_thread.rb
+++ lib/akita/har_logger/writer_thread.rb
@@ -15,42 +15,57 @@ module Akita
         shutdown_mutex = Mutex.new
 
         Thread.new do
+          Rails.logger.debug "AKITA: About to acquire shutdown mutex "\
+                             "#{shutdown_mutex} for file #{out_file_name} "\
+                             "in writer thread #{Thread.current}. "\
+                             "Self-owned? #{shutdown_mutex.owned?}"
           shutdown_mutex.synchronize {
-            File.open(out_file_name, 'w') { |f|
-              # Produce a preamble.
-              f.write <<~EOF.chomp
-                {
-                  "log": {
-                    "version": "1.2",
-                    "creator": {
-                      "name": "Akita HAR logger for Ruby",
-                      "version": "1.0.0"
-                    },
-                    "entries": [
-              EOF
+            begin
+              Rails.logger.debug "AKITA: Acquired shutdown mutex "\
+                                 "#{shutdown_mutex} for file "\
+                                 "#{out_file_name} in writer thread "\
+                                 "#{Thread.current}."
+              File.open(out_file_name, 'w') { |f|
+                # Produce a preamble.
+                f.write <<~EOF.chomp
+                  {
+                    "log": {
+                      "version": "1.2",
+                      "creator": {
+                        "name": "Akita HAR logger for Ruby",
+                        "version": "1.0.0"
+                      },
+                      "entries": [
+                EOF
 
-              first_entry = true
+                first_entry = true
 
-              loop do
-                entry = entry_queue.pop
-                if entry == nil then break end
+                loop do
+                  entry = entry_queue.pop
+                  if entry == nil then break end
 
-                # Emit comma separator if needed.
-                f.puts (first_entry ? '' : ',')
-                first_entry = false
+                  # Emit comma separator if needed.
+                  f.puts (first_entry ? '' : ',')
+                  first_entry = false
 
-                # Emit the dequeued entry.
-                f.write JSON.generate(entry)
-              end
+                  # Emit the dequeued entry.
+                  f.write JSON.generate(entry)
+                end
 
-              # Produce the epilogue.
-              f.write <<~EOF
+                # Produce the epilogue.
+                f.write <<~EOF
 
-                    ]
+                      ]
+                    }
                   }
-                }
-              EOF
-            }
+                EOF
+              }
+            ensure
+              Rails.logger.debug "AKITA: About to release shutdown mutex "\
+                                 "#{shutdown_mutex} for file "\
+                                 "#{out_file_name} in writer thread "\
+                                 "#{Thread.current}."
+            end
           }
         end
 
@@ -59,7 +74,18 @@ module Akita
           # Signal to the consumer that this is the end of the entry stream and
           # wait for the consumer to terminate.
           entry_queue << nil
-          shutdown_mutex.synchronize {}
+          Rails.logger.debug "AKITA: About to acquire shutdown mutex "\
+                             "#{shutdown_mutex} for file #{out_file_name} "\
+                             "while shutting down in #{Thread.current}. "\
+                             "Self-owned? #{shutdown_mutex.owned?}"
+          shutdown_mutex.synchronize {
+            Rails.logger.debug "AKITA: Acquired shutdown mutex "\
+                               "#{shutdown_mutex} for file #{out_file_name} "\
+                               "while shutting down in #{Thread.current}."
+            Rails.logger.debug "AKITA: About to release shutdown mutex "\
+                               "#{shutdown_mutex} for file #{out_file_name} "\
+                               "while shutting down in #{Thread.current}."
+          }
         end
       end
     end
